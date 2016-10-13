3003b39257635fd897ff1e452a2e68d193da87b7
ltysGitHub <287233266@qq.com>
2016-10-13 10:41:42 +0800

    do_with_xml放错地方了

--- app/src/main/java/com/ltys/pw_manager/MainActivity.java
+++ app/src/main/java/com/ltys/pw_manager/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    do_with_xml dwx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
